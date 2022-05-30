e4a01a953f6886e4156ab14ae670c7a93050e80c
Salohiddin98 <salohiddin.rashidov98@gmail.com>
2022-05-30 21:45:17 +0500

    refactor: rename style classes,remove incorrect tag selectors, use tag separation according to rule 3.1, 3.2, 3.3, 3.4, 3.8 in html-and-css.md and use BEM and semantic tags according to rule 1.1, 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -29,19 +29,23 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
 
+  listItem.className="task"
+
   label.innerText=taskString;
-  label.className='task';
+  label.className='task__label';
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className="task__checkbox"
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task__text";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="task__edit-btn edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="task__del-btn delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.className="task__icon"
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
