aac3a535b85712c3d4ac7c503d25ef5edafd5e9d
Salohiddin98 <salohiddin.rashidov98@gmail.com>
2022-06-01 14:55:16 +0500

    refactor: replace tag selector to class selector in js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById('new-task');//Add a new task.
-var addButton=document.getElementsByTagName('button')[0];//first button
+var addButton=document.querySelector('.add__btn');//first button
 var incompleteTaskHolder=document.getElementById('incompleted-tasks');//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById('completed-tasks');//completed-tasks
 
